3eabacbbbdd19725add2fe60031afcabfe15a357
YanakornS <654259002@webmail.npru.ac.th>
2025-03-14 18:46:35 +0700

    asfasf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import BannerSection from './pages/Home/BannerSection'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <BannerSection />
     </>
   )
 }
